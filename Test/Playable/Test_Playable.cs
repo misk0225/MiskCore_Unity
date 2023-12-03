e9a17478eaf749e1494fcd12d481d1141abf8403
misk <yrgna1234@gmail.com>
2023-12-03 12:04:28 +0800

    no message

--- Test/Playable/Test_Playable.cs
+++ Test/Playable/Test_Playable.cs
@@ -19,9 +19,26 @@ namespace MiskCore.Playables.Module.IdleBase
         [SerializeField]
         private Slider _ActionSpeedSlider;
 
+        [SerializeField]
+        private Slider _IncrementXSlider;
+
+        [SerializeField]
+        private Slider _IncrementYSlider;
+
+        [SerializeField]
+        private float _MaxIncrementX;
+
+        [SerializeField]
+        private float _MaxIncrementY;
+
+
+        private IIdlePlayable _CurIdle;
+
+
         public void Awake()
         {
-            _Component.SetIdlePlayable(_IdleSetting.GetIdle(_Component.Graph));
+            _CurIdle = _IdleSetting.GetIdle(_Component.Graph);
+            _Component.SetIdlePlayable(_CurIdle);
         }
 
         public void SetSpeed(float speed)
@@ -35,6 +52,20 @@ namespace MiskCore.Playables.Module.IdleBase
         public void ClipAction()
         {
             _Component.ActionOnceAnimation(new ClipActionOncePlayable(_Component.Graph, _ActionClip, 0.2f, 0.2f), null, _ActionSpeedSlider.value);
+        }
+
+        public void SetIncrement()
+        {
+            if (_CurIdle == null) return;
+
+            if (_CurIdle is IncrementIdlePlayable)
+            {
+                (_CurIdle as IncrementIdlePlayable).UpdateWeight(_IncrementXSlider.value * _MaxIncrementX);
+            }
+            else if (_CurIdle is Increment2DIdlePlayable)
+            {
+                (_CurIdle as Increment2DIdlePlayable).UpdateWeight(_IncrementXSlider.value * _MaxIncrementX, _IncrementYSlider.value * _MaxIncrementY);
+            }
         }
     }
 
